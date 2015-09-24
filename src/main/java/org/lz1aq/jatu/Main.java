5b2fc7529a49f6367f0ac01671638ca73496c83c
levkov_cha <ch.levkov@gmail.com>
2015-09-24 14:20:31 +0200

    jSSC library - for easy work with serial ports

--- src/main/java/org/lz1aq/jatu/Main.java
+++ src/main/java/org/lz1aq/jatu/Main.java
@@ -6,6 +6,8 @@
 package org.lz1aq.jatu;
 
 
+import jssc.SerialPort;
+import jssc.SerialPortException;
 import org.lz1aq.rig_interfaces.I_Radio;
 
 
@@ -16,21 +18,35 @@ public class Main
 {
     public static void main(String args[])
     {  
-        JythonObjectFactory f2 = new JythonObjectFactory(I_Radio.class, "icom", "Icom");
-        I_Radio radio = (I_Radio) f2.createObject();
-        
-        byte[] command;
-        
-          
-        command = radio.encodeSetFreq(7100000, 1);
-        
-        for(int i=0; i<command.length; i++) 
-          System.err.println(String.format("%X", command[i]));
-        
-        
-        command = radio.encodeSetMode(RadioModes.ECSSLSB.toString(), 1);
-        for(int i=0; i<command.length; i++) 
-          System.err.println(String.format("%X", command[i]));
+//        JythonObjectFactory f2 = new JythonObjectFactory(I_Radio.class, "icom", "Icom");
+//        I_Radio radio = (I_Radio) f2.createObject();
+//        
+//        byte[] command;
+//        
+//          
+//        command = radio.encodeSetFreq(7100000, 1);
+//        
+//        for(int i=0; i<command.length; i++) 
+//          System.err.println(String.format("%X", command[i]));
+//        
+//        
+//        command = radio.encodeSetMode(RadioModes.ECSSLSB.toString(), 1);
+//        for(int i=0; i<command.length; i++) 
+//          System.err.println(String.format("%X", command[i]));
+      
+      
+      SerialPort serialPort = new SerialPort("COM1");
+      try
+      {
+        System.out.println("Port opened: " + serialPort.openPort());
+        System.out.println("Params setted: " + serialPort.setParams(9600, 8, 1, 0));
+        System.out.println("\"Hello World!!!\" successfully writen to port: " + serialPort.writeBytes("Hello World!!!".getBytes()));
+        System.out.println("Port closed: " + serialPort.closePort());
+      } catch (SerialPortException ex)
+      {
+        System.out.println(ex);
+      }
+
 
     }
 }
